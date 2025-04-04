c8d61a5e25a456db98f414edc61c6ef81b55055b
kenny <kennygarcia15@csu.fullerton.edu>
2025-04-03 23:53:41 -0700

    Completed mini challenge

--- 03/mini_project-2/src/components/Practice2.jsx
+++ 03/mini_project-2/src/components/Practice2.jsx
@@ -4,19 +4,27 @@ import { useState } from 'react'
 import React from 'react'
 
 const Practice2 = () => { 
-    let counter = 0;
-    function counterFormula() {
-        let [counter, setCounter] = useState(0);
-        setCounter(counter + 1)
+    let [count, setCount] = useState(0);
+
+    function add() {
+        setCount(prevCount => prevCount + 1)
+        console.log("add")
+        console.log(count)
+    }
+
+    function minus() {
+        setCount(prevCount => prevCount - 1)
+        console.log("minus")
+        console.log(count)
     }
 
   return (
     <main className="container">
         <h1>How many times will Bob say "state" in this section?</h1>
         <div className="counter">
-            <button className="minus" aria-label="Decrease count">-</button>
-            <h2 className="count" onClick={counterFormula}>{counter}</h2>
-            <button className="plus" aria-label="Increase count">+</button>
+            <button className="minus" onClick={minus} aria-label="Decrease count">-</button>
+            <h2 className="count">{count}</h2>
+            <button className="plus" onClick={add} aria-label="Increase count">+</button>
         </div>
     </main>
   )
