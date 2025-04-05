b35eddf7369d3f6d4af2c3d9661574db8dc86ba6
bebopkenny <kennygarcia15@csu.fullerton.edu>
2025-04-04 22:11:23 -0700

    Added a much faster way to get the email and password info for a form section

--- 03/mini-project-3/src/components/SignUp.jsx
+++ 03/mini-project-3/src/components/SignUp.jsx
@@ -3,6 +3,37 @@ import './SignUp.css'
 import React from 'react'
 
 const SignUp = () => {
+
+    function signUp() { // much faster and cleaner way to capture the email and send it to a database 
+        const email = FormData.get("email") // gets the name="email"
+        const password = FormData.get("password") // gets the name="password"
+    }
+
+  return ( // Don't forget to add a function to action for this to work  
+    <section>
+        <h1>Signup form</h1>
+        <form action={signUp}> 
+            <label htmlFor="email">Email:</label>
+            <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+            <br />
+
+            <label htmlFor="password">Password:</label>
+            <input id="password" type="password" name="password" placeholder="Stars123!"/>
+            <br />
+            <button>Submit</button>
+        </form>
+    </section>
+  )
+}
+
+export default SignUp
+
+/*
+import './SignUp.css'
+
+import React from 'react'
+
+const SignUp = () => {
     function handleSubmit(event) {
         event.preventDefault()
         console.log("submitted")
@@ -33,3 +64,5 @@ const SignUp = () => {
 }
 
 export default SignUp
+
+*/
