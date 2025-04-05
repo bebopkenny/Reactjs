13a9b08a467a3edbdcdf9bb65f731acfb53c12e1
kenny <kennygarcia15@csu.fullerton.edu>
2025-04-05 00:21:40 -0700

    Learned how to deal with radio inputs

--- 03/mini-project-3/src/components/SignUp.jsx
+++ 03/mini-project-3/src/components/SignUp.jsx
@@ -3,22 +3,49 @@ import React from 'react'
 
 const SignUp = () => {
 
-    function signUp() { // much faster and cleaner way to capture the email and send it to a database 
-        const email = FormData.get("email") // gets the name="email"
-        const password = FormData.get("password") // gets the name="password"
+    function signUp(event) { // much faster and cleaner way to capture the email and send it to a database 
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        let user_email = formData.get("email")
+        let user_password = formData.get("password")
+    }
+
+    function employmentStatus(event) {
+      const selected = event.target.value; // gets the value from the selected radio
+      console.log("Employment status: ", selected);
     }
 
   return ( // Don't forget to add a function to action for this to work  
     <section>
         <h1>Signup form</h1>
-        <form action={signUp}> 
+        <form onSubmit={signUp}> 
             <label htmlFor="email">Email:</label>
-            <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+            <input id="email" defaultValue="joe@schmore.com" type="email" name="email" placeholder="joe@schmoe.com" />
             <br />
 
             <label htmlFor="password">Password:</label>
             <input id="password" type="password" name="password" placeholder="Stars123!"/>
             <br />
+            
+            <label htmlFor="description">Description</label>
+            <textarea id="description" name="description" defaultValue="This is a description"></textarea>
+            
+            <fieldset>
+              <legend>Employment Status:</legend>
+              <label>
+                <input onChange={employmentStatus} type="radio" name="employmentStatus" value="unemployed"/>
+                Unemployed
+              </label>
+              <label>
+                <input onChange={employmentStatus} type="radio" name="employmentStatus" value="part-time"/>
+                Part-Time
+              </label>
+              <label>
+                <input onChange={employmentStatus} type="radio" name="employmentStatus" value="full-time"/>
+                Full-Time
+              </label>
+            </fieldset>
+            
             <button>Submit</button>
         </form>
     </section>
@@ -26,6 +53,10 @@ const SignUp = () => {
 }
 
 export default SignUp
+
+
+
+
 
 /*
 import './SignUp.css'
