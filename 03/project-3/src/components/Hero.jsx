f887822f2c954de26a6880f9bd65224500795718
kenny <kennygarcia15@csu.fullerton.edu>
2025-04-04 22:52:44 -0700

    Updated inputIngredient from Hero.jsx

--- 03/project-3/src/components/Hero.jsx
+++ 03/project-3/src/components/Hero.jsx
@@ -1,19 +1,47 @@
 import './Hero.css'
-
+import { useState } from 'react';
 import React from 'react'
 
 const Hero = () => {
+  let [foods, setFoods] = useState([]) // created an empty list for foods with a useState setFoods so we can modify the list
+
+    function inputIngredient(event) { // get the user input from the form
+      event.preventDefault(); // doesn't refresh the page
+      const formData = new FormData(event.currentTarget); 
+      let user_ingredient = formData.get("ingredient"); // gets the input name
+      setFoods(prevFoods => [...prevFoods, user_ingredient]);
+    }
+
   return (
     <main className="main">
-        <form className="add-ingredient-form">
+        <form onSubmit={inputIngredient} className="add-ingredient-form">
             <input 
                 type="type"
                 placeholder="e.g. oregano"
-                aria-label="Add ingredient" />
+                aria-label="Add ingredient" 
+                name="ingredient"
+            />
             <button>Add ingredient</button>
         </form>
+        <ul>
+          {foods.map((item, value) => ( // map over the foods list 
+            <li key={value}>{item}</li> // display the items as a <li></li>
+          ))}
+        </ul>
     </main>
-  )
+  );
 }
 
 export default Hero;
+
+/*
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log("Form submitted!");
+  const formData = new FormData(event.currentTarget)
+  const newIngredient = formData.get("ingredient") // get the item of what the user typed in
+  setFoods(prevFoods => [...prevFoods, newIngredient]) // going to add the ingredient the user typed in onto the foods list
+}
+
+*/
