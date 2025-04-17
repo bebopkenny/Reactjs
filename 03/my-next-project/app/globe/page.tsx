6b5cb75976cf9c98c9cf34fa410651001d4f62b2
kenny <kennygarcia15@csu.fullerton.edu>
2025-04-16 23:16:45 -0700

    Updated with abort control ref

--- 03/my-next-project/app/globe/page.tsx
+++ 03/my-next-project/app/globe/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { abort } from 'process';
 import { useState, useEffect, useRef } from 'react'
 
   interface Post {
@@ -14,17 +15,26 @@ const page = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [page, setPage] = useState(0);
 
-  const abortControllerRef = useRef<AbortController | null>(null)>
+  const abortControllerRef = useRef<AbortController | null>(null)
 
   useEffect(() => {
     const fetchPosts = async() => { // wait until the data has arrived
+      abortControllerRef.current?.abort(); // Abort request if it exist
+      abortControllerRef.current = new AbortController();
       setIsLoading(true);
 
       try {  // trying to fetch the data if it fails then runs catch for any error
-        const response = await fetch(`${BASE_URL}/posts?page=${page}`); // wait until the data has loaded from the website
+        const response = await fetch(`${BASE_URL}/posts?page=${page}`, { // wait until the data has loaded from the website
+          signal: abortControllerRef.current?.signal, 
+        }); 
         const posts = await response.json() as Post[]; // turn the response to json nad put it in Post
         setPosts(posts) // put posts to the setPost
       } catch (e: any) {
+        if (e.name === "AbortError") {
+          console.log("Aborted");
+          return;
+        }
+        
         setError(e);
       } finally {
         setIsLoading(false);
@@ -34,9 +44,6 @@ const page = () => {
     fetchPosts();
   }, [page]);
   
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
   if (error) {
     return <div>Something went wrong! Please try again.</div>;
@@ -46,11 +53,14 @@ const page = () => {
     <div>
       <h1 className="mb-4 text-2xl">Data Fetching in React</h1>
       <button onClick={() => setPage(page + 1)}>Increase page ({page})</button>
+      {isLoading && <div>Loading...</div>}
+      {!isLoading && (
       <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
       </ul>
+      )}
     </div>
   )
 }
